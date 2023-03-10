c4ce1ff1157a14fed7437915dd9d6b633d401f1c
Ahmed Bashir <bisha.mail.box@gmail.com>
2023-03-10 20:00:40 +0200

    feat(router): add create post to posts router

--- src/server/api/routers/posts.ts
+++ src/server/api/routers/posts.ts
@@ -1,4 +1,13 @@
 import { z } from "zod";
 import { createTRPCRouter, publicProcedure } from "../trpc";
 
-export const PostsRouter = createTRPCRouter({});
+export const PostsRouter = createTRPCRouter({
+  createPost: publicProcedure
+    .input(z.object({ title: z.string() }))
+    .mutation(async ({ input, ctx }) => {
+      const result = await ctx.prisma.post.create({
+        data: { title: input.title },
+      });
+      return result;
+    }),
+});
