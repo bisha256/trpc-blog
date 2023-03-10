597372962836206078b55ec954c34f9add922944
Ahmed Bashir <bisha.mail.box@gmail.com>
2023-03-10 22:07:16 +0200

    feat(router): add create comment to comments router

--- src/server/api/routers/comments.ts
+++ src/server/api/routers/comments.ts
@@ -1,3 +1,11 @@
-import { createTRPCRouter } from "../trpc";
+import { z } from "zod";
+import { createTRPCRouter, publicProcedure } from "../trpc";
 
-export const commentsRouter = createTRPCRouter({});
+export const commentsRouter = createTRPCRouter({
+  createComment: publicProcedure
+    .input(z.object({ body: z.string(), postId: z.string() }))
+    .mutation(({ input, ctx }) => {
+      const comment = ctx.prisma.comment.create({ data: input });
+      return comment;
+    }),
+});
