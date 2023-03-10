18c502c93e829a9274a1804c0d3be880c6a7690c
Ahmed Bashir <bisha.mail.box@gmail.com>
2023-03-11 00:30:25 +0200

    feat(pages): add title to post details page

--- src/pages/posts/[id].tsx
+++ src/pages/posts/[id].tsx
@@ -1,4 +1,5 @@
 import { NextPage } from "next";
+import Head from "next/head";
 import { useRouter } from "next/router";
 import { api } from "~/utils/api";
 import CommentForm from "./components/CommentForm";
@@ -10,11 +11,16 @@ const Post: NextPage = () => {
 
   if (data)
     return (
-      <div>
-        <h3>{data.title}</h3>
-        <Comments comments={data.comments} />
-        <CommentForm postId={data.id} />
-      </div>
+      <>
+        <Head>
+          <title>{data.title}</title>
+        </Head>
+        <div>
+          <h3>{data.title}</h3>
+          <Comments comments={data.comments} />
+          <CommentForm postId={data.id} />
+        </div>
+      </>
     );
   else return <div>Not Found</div>;
 };
